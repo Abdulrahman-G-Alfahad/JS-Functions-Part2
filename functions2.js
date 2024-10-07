d9dd920a381f9e85059660a9d4dafb357b8b0122
unknown <abdulrahman.g.alfahad@gmail.com>
2024-10-07 11:46:37 +0300

    Completed all tasks

---
+++ functions2.js
@@ -0,0 +1,50 @@
+/**
+ * isOdd(n):
+ * - receives a number n
+ * - returns true if it's odd, false otherwise
+ *
+ * e.g.
+ * isOdd(7) -> true
+ * isOdd(10) -> false
+ */
+function isOdd(n) {
+    // Your code here
+    return !((n%2) === 0);
+}
+
+/**
+ * oddsSmallerThan(n):
+ * - receives a number n
+ * - returns the number of ODD numbers smaller than n
+ *
+ * e.g.
+ * oddsSmallerThan(7) -> 3
+ * oddsSmallerThan(15) -> 7
+ */
+function oddsSmallerThan(n) {
+    // Your code here
+    const numberOfOdds = Math.floor(n/2);
+    return numberOfOdds;
+}
+
+/**
+ * squareOrDouble(n):
+ * - receives a number n
+ * - returns its square if it's odd
+ * - returns its double if it's even
+ *
+ * e.g.
+ * squareOrDouble(16) -> 32
+ * squareOrDouble(9) -> 81
+ */
+function squareOrDouble(n) {
+    // Your code here
+    let result = 0;
+    if (n%2 === 0) result = n*2;
+    else result = n*n;
+
+    return result;
+}
+
+  console.log(squareOrDouble(16));
+  console.log(squareOrDouble(9));
